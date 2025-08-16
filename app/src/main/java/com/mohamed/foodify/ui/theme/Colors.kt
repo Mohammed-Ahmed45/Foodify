009613fd23fa04713706d6c34ada3db4fd6b9972
MohamedAhmed <mo9958158@gmail.com>
2025-08-16 16:39:06 +0300

    Project setup

---
+++ app/src/main/java/com/mohamed/foodify/ui/theme/Colors.kt
@@ -0,0 +1,10 @@
+package com.mohamed.foodify.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object Colors {
+    val Blue = Color(0xFF0048FF)
+    val LightBlue = Color(0xFF407BFF)
+    val DarkGreen = Color(0xFF156651)
+    val Red = Color(0xFFE60023)
+}
